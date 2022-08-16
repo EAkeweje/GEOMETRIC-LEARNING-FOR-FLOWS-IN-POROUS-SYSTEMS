55ca8125c3fb4b8016301d68030dbde9361981ff
Vsevolod Avilkin <vsevolod.avilkin@skoltech.ru>
2022-08-16 22:40:28 +0300

    setting up my branch. Added Metropolis script and sweep for wandb

---
+++ LS_Algorithm.py
@@ -0,0 +1,254 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import networkx as nx
+import torch
+from torch.autograd import Variable
+import torch.optim as optim
+import torch.nn as nn
+from torch.utils.data import Dataset
+from torch.utils.data.sampler import SubsetRandomSampler
+import PIL
+import pandas as pd
+from tqdm.notebook import tqdm
+import json
+import time
+import cv2
+import random
+import glob
+import copy
+
+#####custom functions########
+import sys
+import os
+sys.path.append(os.getcwd()+'/scripts')
+
+import utils
+
+## Custom fluid flow data object
+class FluidDatasetPlus(Dataset):
+    """
+    Fluid in porous media dataset.
+    This pytorch dataset object is an extension of FluidDataset.
+    While the earlier version only supports randomly chosen sensor (gauge) positions, 
+    this version allows for specification of gauge positions. This is more applicable where the objective is to optimize the sensor (gauge) placement.
+    """
+
+    def __init__(self, img_path, root_dir, ks, num_gauge = 0, gauge_pos = None, rnd_seed = 42):
+        """
+        Args:
+            img_path (string): Path to the porous structure image.
+            root_dir (string): Directory with all the velocity data.
+            ks (int): coarse graining grid size. If no coarse graining is required, set ks to 1
+            num_gauge (int): number of sensors
+            gauge_pos (list): list of sensor position turples (x,y)
+        """
+        self.img_path = img_path
+        self.ks = ks
+        self.root_dir = root_dir
+        self.num_gauge = num_gauge
+
+        ##get pore image array
+        an_image = PIL.Image.open(self.img_path)
+        image_sequence = an_image.getdata()
+        image_array = np.array(image_sequence)/255
+        k = np.asarray(an_image)[:,:,0]/255
+        #adjustment so that the pore spaces are mapped to 0 and the solid immovable parts mapped to 1
+        self.pore_array = np.ones_like(k) - k
+        #get list velocity data
+        self.vel_data = sorted(glob.glob(self.root_dir+'/*.dat'))
+        ##coarse grain the system
+        self.CG_array = utils.coarse_grain(np.rot90(self.pore_array), self.ks)
+        #'install' sensors at pore walls... get the wall boundaries
+        #To condition same condition as that used for the graphing image
+        CG_array = np.where(self.CG_array <= self.ks**2/2, 0, 1)
+        #get total space from working array
+        self.total_space = utils.get_total_space(CG_array)
+        #if gauge positions not given, pick gauge positions randomly from pore walls
+        if not gauge_pos:
+            assert self.num_gauge != 0, "Since gauge_pos = None, ensure to specify the number of gauges the program should use. Set gauge > 0"
+            boundary_points = utils.get_bounds(1-CG_array)
+            #random select gauge spots
+            random.seed(rnd_seed)
+            self.gauge_space = random.sample(boundary_points, self.num_gauge)
+        else:
+            #check if no sensor is placed on pore solid
+            check = [_ in self.total_space for _ in gauge_pos]
+            assert all(check), f"Adjust gauge_pos. The sensor positions in the following indices are not positioned in a pore space: {[i for i, x in enumerate(check) if x == False]}"
+            self.gauge_space = gauge_pos
+            self.num_gauge = len(gauge_pos)
+        # assert self.num_gauge == self.gauge_space, 'Number of guages generated is not equal to the number specified'
+        #identify the no remaining no gauge spots (to estimate)
+        self.no_gauge_space = copy.deepcopy(self.total_space)
+        dup_sensor = list()
+        for b_ in self.gauge_space:
+            try:
+                self.no_gauge_space.remove(b_)
+            except:
+                dup_sensor.append(b_)
+        for b_ in dup_sensor:
+            self.gauge_space.remove(b_)
+
+    def __len__(self):
+        return len(self.vel_data)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        #get velocity field data
+        f = np.loadtxt(self.vel_data[idx])
+        f = np.reshape(f,(256,256,2))
+        f = np.rot90(f)
+        #coarse graining velocity data
+        CG_f_0 = utils.coarse_grain(f[:,:,0],self.ks)/(self.ks*self.ks)
+        CG_f_1 = utils.coarse_grain(f[:,:,1],self.ks)/(self.ks*self.ks)
+        #partitions velocity data
+        vel_x_ngs = [CG_f_0[i] for i in self.no_gauge_space] #velocity (x-dimension) at no sensor
+        vel_y_ngs = [CG_f_1[i] for i in self.no_gauge_space] #velocity (y-dimension) at no sensor
+        vel_x_gs = [CG_f_0[i] for i in self.gauge_space] #velocity (x-dimension) from sensor
+        vel_y_gs = [CG_f_1[i] for i in self.gauge_space] #velocity (y-dimension) from sensor
+        
+        return torch.tensor([vel_x_gs,vel_y_gs]), torch.tensor([vel_x_ngs,vel_y_ngs]) #returns sensor data (tensor), no sensor data (tensor)
+
+##LS solver
+class LSReconstrutor():
+    def __init__(self, dataset, data_split = 0.3, shuffle = True, seed = 42):
+
+        self.data_split = data_split
+        self.shuffle = True
+        self.data = dataset
+        self.seed = seed
+
+        dataset_size = len(self.data)
+        indices = list(range(dataset_size))
+        split = int(np.floor(self.data_split * dataset_size))
+        if self.shuffle :
+            np.random.seed(seed)
+            np.random.shuffle(indices)
+        train_indices, val_indices = indices[split:], indices[:split]
+
+        # Creating PT data samplers and loaders:
+        train_sampler = SubsetRandomSampler(train_indices, torch.random.manual_seed(seed))
+        valid_sampler = SubsetRandomSampler(val_indices, torch.random.manual_seed(seed))
+        #Creating dataloaders
+        self.train_loader = torch.utils.data.DataLoader(dataset, batch_size=1, sampler=train_sampler)
+        self.validation_loader = torch.utils.data.DataLoader(dataset, batch_size=1, sampler=valid_sampler)
+
+    def fit(self):
+        #(training) data loading
+        for i, (x,y) in enumerate(self.train_loader):
+            if i == 0:
+                output_stack = y
+                input_stack = x
+            else:
+                output_stack = torch.cat([output_stack,y])
+                input_stack = torch.cat([input_stack,x])
+
+        #vectorization of data
+        output_dim = output_stack.shape[0]
+        input_dim = input_stack.shape[0]
+        output = output_stack.view(output_dim,-1).T
+        input = input_stack.view(input_dim,-1).T
+
+        #compute coefficient matrices
+        self.coef_mat = output @ torch.linalg.pinv(input)
+
+        return self.coef_mat
+
+    def predict(self, error_type = None):
+        #Inference on validation data
+        '''
+        pred = A @ X; 
+        where A = reconstruction operator, X = sensor measurement
+        Also Computes the error; limited to NME, R2 and NFE.
+        '''
+        for i, (x,y) in enumerate(self.validation_loader):
+            x_dim = x.shape[0]
+            y_dim = y.shape[0]
+            #vectorize validation each snapshot
+            x = x.view(x_dim,-1).T
+            y = y.view(y_dim,-1).T
+            if i == 0:
+                target_stack = y.T
+                pred_stack = (self.coef_mat @ x).T
+            else:
+                target_stack = torch.cat([target_stack,y.T])
+                pred_stack = torch.cat([pred_stack, (self.coef_mat @ x).T])
+
+        self.target = target_stack
+        self.pred = pred_stack
+
+        #losses
+        self.NME = self.NME_loss()
+        self.NFE = self.NFE_loss()
+        self.r2 = self.r2_loss()
+
+        return self.target, self.pred #returns groundtruth and prediction
+
+  #scorings
+    def NME_loss(self):
+        error = torch.linalg.norm(self.target - self.pred)/torch.linalg.norm(self.target)
+        return error
+    
+    def r2_loss(self):
+        target_mean = torch.mean(self.target)
+        ss_tot = torch.sum((self.target - target_mean) ** 2)
+        ss_res = torch.sum((self.target - self.pred) ** 2)
+        r2 = 1 - ss_res / ss_tot
+        return r2
+
+    def NFE_loss(self):
+        target = self.target - self.target.mean(dim =0)
+        pred = self.pred - self.target.mean(dim =0)
+        error = torch.linalg.norm(target - pred)/torch.linalg.norm(target)
+        return error
+
+#Helper function to get node positions
+def get_node_pos(G, node_list):
+    '''
+    inputs:::
+    G: nx Graph
+    node_list (list): A list of nodes to place gauges
+
+    return:::
+    A list of node position in the porous structure
+    '''
+    pos = []
+    for i in node_list:
+        pos.append(G.nodes(data = 'node_pos')[i])
+    return pos
+
+def get_nme(nodes, ks, G):
+    #extract sensor (nodes) positions
+    gauge_pos = get_node_pos(G, nodes)
+
+    try:
+        dataset = FluidDatasetPlus(img_path="./Dataset/1.png",
+                                   root_dir = "./Dataset",
+                                   ks = ks, gauge_pos = gauge_pos)
+    except AssertionError:
+        if (29, 71) in gauge_pos:
+            gauge_pos.remove((29, 71))
+        if (109, 36) in gauge_pos:
+            gauge_pos.remove((109, 36))
+        dataset = FluidDatasetPlus(img_path="./Dataset/1.png",
+                                   root_dir = "./Dataset",
+                                   ks = ks, gauge_pos = gauge_pos)
+
+    # print(f'Selected {dataset.num_gauge} nodes')
+
+    LS = LSReconstrutor(dataset)
+    LS.fit()
+    LS.predict()
+    return LS.NME
+
+if __name__ == '__main__':
+    #get sensor graph
+    ks = 2 #coarse_graining grid size
+    G = nx.read_gpickle(f"pore_network_0{ks}.gpickle")
+
+    #select nodes
+    nodes = random.sample(list(np.arange(310)), 100)
+
+
+    print(get_nme(nodes, ks, G))
+
---
+++ Metropolis_MC.py
@@ -0,0 +1,175 @@
+## Import libraries
+
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import trange, tqdm
+from tqdm import tqdm, trange
+import wandb
+import networkx as nx
+
+from LS_Algorithm import get_nme
+
+## Initialization and supplementary functions
+
+def adj_matrix(G):
+    '''
+    creates adj and neigbors:
+    adj: 2D adjacency matrix of a graph
+    neighbors: list of lists of neighbors (indexes from 0 to N-1, where N is a number of nodes)
+    '''
+    adj = nx.to_numpy_array(G)
+ 
+    neighbors = []
+    for i in range(nx.number_of_nodes(G)):
+        neighbors_ = [neighbor for neighbor in nx.neighbors(G, i)]
+        neighbors.append(neighbors_)
+    return adj, neighbors
+
+
+def system_initialization(G, N_sensors):
+    '''
+    - Uses previously stated function adj_matrix to initialize 
+    all needed information about graph
+    - Initializes sensors positions
+    ----------------------------------
+    G: graph
+    adj: adjacency matrix
+    N_sensors: number of sensors
+    sensors: ndarray of sensors' indexes 
+    '''
+    n = nx.number_of_edges(G)
+    N_nodes = nx.number_of_nodes(G)
+
+    adj, neighbors = adj_matrix(G)
+    sensors = np.array([(N_nodes*i)//N_sensors for i in range(N_sensors)])
+
+    return adj, neighbors, sensors
+
+## Metropolis algorithm
+
+def step(sensors, E_tot, Statistics, neighbors, adj):
+    '''
+    one step of a Metropolis algorithm cycle:
+
+    1. shifts one random sensor to the neigboring node
+    2. calculates the difference in energy
+    3. accepts\rejects new location of the sensor
+    '''
+
+    # choose the random sensor
+    choice = rng.choice
+    old_sensor_number = choice(np.arange(N_sensors))
+    old_node_number = sensors[old_sensor_number]
+
+    # skip to the next step if there are no neighbors
+    if len(neighbors[old_node_number]) == 0:
+        # no neighbors
+        Statistics[0] +=1 
+        return sensors, E_tot, Statistics
+
+    # choose the random neighboring node of the chosen one
+    num_of_neighbors = len(neighbors[old_node_number])
+    neighboring_loc = choice(np.arange(num_of_neighbors))
+    new_node_number = neighbors[old_node_number][neighboring_loc]
+
+    # skip to the next step if the chosen node is occupied
+    if new_node_number in sensors:
+        Statistics[1] +=1
+        return sensors, E_tot, Statistics
+
+    #shift the sensor
+    # sensors_new = np.delete(sensors, old_sensor_number) 
+    # sensors_new = np.append(sensors_new, new_node_number)
+    sensors_new = sensors.copy()
+    sensors_new[old_sensor_number] = new_node_number
+    # calculate the difference in energy
+    E_new = get_nme(sensors_new, ks, G)
+    dE = E_new - E_tot
+    # accept/reject
+    dp = np.exp(-dE/T)
+    rand = rng.random()
+    wandb.log({'E/random': rand, 'E/acceptance probability': dp}, commit=False)
+    if dp > rand:
+        # accept
+        E_tot = E_new
+        Statistics[2] +=1
+        return sensors_new, E_tot, Statistics
+    else:
+        # reject
+        #return the sensor back
+        Statistics[3] +=1
+        return sensors, E_tot, Statistics
+
+    
+def cycle(sensors, E_tot, neighbors, adj, steps):
+    '''
+    cycle of metropolis algorithm's steps
+    stores information about energy levels during the simulation
+    '''
+
+    Statistics = np.array([0.,  # 'no_neighbors'
+                        0.,  # 'occupied'
+                        0.,  # 'accepted'
+                        0.]) # 'rejected'
+
+    best_sensor_loc = sensors.copy()
+    E_min = E_tot
+    # best_step = 0
+
+
+    for i in trange(1,steps+1):
+        sensors, E_tot, Statistics = step(sensors, E_tot, 
+                                          Statistics, neighbors, adj)
+        if E_tot < E_min:
+            best_sensor_loc = sensors.copy()
+            
+            E_min = E_tot
+            # best_step = i
+            wandb.log({'E/Emin': E_min, 'Best sensor location': best_sensor_loc}, commit=False)
+        
+        wandb.log({'E/E': E_tot, 'Statistics/no_neighbors':Statistics[0], 'Statistics/occupied':Statistics[1], 
+                   'Statistics/accepted':Statistics[2], 'Statistics/rejected':Statistics[3]})
+        
+    Statistics /= steps
+    return sensors, E_tot, Statistics,\
+           best_sensor_loc, E_min
+
+def simulation(adj, neighbors, sensors, steps):
+    '''
+    does the simulation and returns numerical results
+    '''
+    E_tot = get_nme(sensors, ks, G)
+    sensors, E_tot, Statistics, best_sensor_loc, E_min =\
+        cycle(sensors, E_tot, neighbors, adj, steps)
+    print(f'E min: {E_min}\n best location:{best_sensor_loc}')
+    # verification of right energy calculation
+    E_fin = get_nme(sensors, ks, G)
+    assert np.isclose(E_fin, E_tot), f'E_fin={E_fin}, E_tot={E_tot}'
+
+    return sensors, Statistics, \
+           best_sensor_loc, E_min
+
+if __name__ == '__main__':
+    ks = 2 #coarse_graining grid size
+    G = nx.read_gpickle(f"pore_network_0{ks}.gpickle")
+    rng = np.random.RandomState(42)
+    hyperparameter_defaults = dict(
+        T=1,
+        N_sensors=200,
+        steps = 10**4
+        )
+    wandb.init(config=hyperparameter_defaults)
+
+    config = wandb.config
+    T = config.T
+    N_sensors = config.N_sensors
+    steps = config.steps
+    
+    adj, neighbors, sensors = system_initialization(G, N_sensors)
+    sensors, Statistics, best_sensor_loc, E_min =\
+        simulation(adj, neighbors, sensors, steps)
+
+    data = {"best_location":best_sensor_loc}
+    df = pd.DataFrame(data)
+    best_loc_table = wandb.Table(data=df)
+    wandb.log({'best location table':best_loc_table}, commit=False)
